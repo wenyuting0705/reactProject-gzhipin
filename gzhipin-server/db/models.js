676534b7e2773481f55dd6d4e80331fd19114af7
wenyuting0705 <wenyuting_0705@163.com>
2018-08-13 14:55:43 +0800

    init project

---
+++ gzhipin-server/db/models.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost:27017/gzhipin')
+const conn=mongoose.connection;
+conn.on('connected',function () {
+  console.log('db connect success!!!')
+})
+
+const userSchema=mongoose.Schema({
+  username: {type: String, required: true}, // 用户名
+  password: {type: String, required: true}, // 密码
+  type: {type: String, required: true}, // 用户类型: dashen/laoban
+  header: {type: String}, // 头像名称
+  post: {type: String}, // 职位
+  info: {type: String}, // 个人或职位简介
+  company: {type: String}, // 公司名称
+  salary: {type: String} // 工资
+});
+const UserModel=mongoose.model('users',userSchema)
+exports.UserModel=UserModel;
---
+++ gzhipin-server/routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+const md5=require('blueimp-md5')
+const {UserModel} = require('../db/models')
+const filter={password:0}
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.post('/register',(req,res)=>{
+  const {username,password,type}=req.body;
+
+  UserModel.findOne({username},(err,userDoc)=>{
+    if(userDoc){
+      res.send({code:1,msg:'用户名已存在，请重新注册！'})
+    }else {
+      new UserModel({username,password:md5(password),type}).save((err,userDoc)=> {
+        res.cookie('userid',userDoc._id,{maxAge:1000*60*60*24*7})
+        res.send({code:0,data:{_id:userDoc._id,username,type}})
+      })
+    }
+  })
+})
+router.post('/login',(req,res)=>{
+  const {username,password}=req.body;
+  UserModel.findOne({username,password:md5(password)},filter,(err,userDoc)=>{
+    if(!userDoc){
+      res.send({code:1,msg:'用户名或密码错误！！！'})
+    }else {
+      res.cookie('userid',userDoc._id,{maxAge:1000*60*60*24*7})
+      res.send({code:0,data:{userDoc}})
+    }
+  })
+})
+module.exports = router;
